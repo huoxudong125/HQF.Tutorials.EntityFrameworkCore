bf8d4dd38f1b3589895eb61efaf618c16484d1cf
huoxudong125 <423huo@163.com>
2017-07-26 18:06:11 +0800

    Init Solution

---
+++ Tests/HQF.Tutorials.EntityFrameworkCore.XUnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace HQF.Tutorials.EntityFrameworkCore.XUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
